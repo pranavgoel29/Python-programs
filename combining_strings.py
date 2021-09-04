fd15333789162b6eaf9056f82d0b898e6e1b098d
pranavgoel29 <pranavgoel29@gmail.com>
2021-09-05 00:37:36 +0530

    Added combining_strings.py

---
+++ combining_strings.py
@@ -0,0 +1,13 @@
+first_name = "Ron"
+last_name = "Weasley"
+full_name = first_name + " " +last_name
+
+print(full_name)
+
+# We can also use `title` method here
+
+first_name = "ron"
+last_name = "weasley"
+full_name = first_name + " " +last_name
+
+print("\nHello, " + full_name.title())
