1175ec69c620161b7eed2d45dbbb01d6e17f0db3
pranavgoel29 <pranavgoel29@gmail.com>
2021-09-05 00:27:38 +0530

    Added strings.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+string = "harry potter"
+
+# `title` method change the first letter of every word to upper case.
+
+print(string.title())       #the dot(.) method tell python to act upon the var named `string`
+
+#There are many other methods that change the case of the string like `upper` and `lower`.
+
+print(string.upper())           # `upper` chages every letter of the string to capital form.
+print(string.lower())
