c051187a9c9ed0c7f402356c45780277d1554bad
pranavgoel29 <pranavgoel29@gmail.com>
2021-09-07 12:24:58 +0530

    Added if_else_condition.py

---
+++ if_else_condition.py
@@ -0,0 +1,14 @@
+# Program to show how if-else statement works.
+
+a = int(input())
+b = int(input())
+
+c = a+b
+
+if (c>5):
+    print("The sum is greater than 5")
+else:
+    print("The sum is smaller than 5")
+
+
+print("If-else statement is completed.")
