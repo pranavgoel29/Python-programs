996c725128f3ea8e8e79222f1a344139ef206d79
pranavgoel29 <pranavgoel29@gmail.com>
2021-12-25 23:43:02 +0530

    added pandas folder and connected and added mysql program

--- My SQL/mysql_prog_1.py
+++ My SQL/mysql_prog_1.py
@@ -1,7 +1,26 @@
 import mysql.connector
-conn_object = mysql.connector.connect(host="localhost", user = "root", passwd = "root")
-# Cur_object
+import os
 
-print(conn_object)
+os.system("cls")
 
+db = mysql.connector.connect(
+    host="localhost",
+    user = "root",
+    passwd = "root",
+    database = "testdatabase"
+    )
  
+mycursor = db.cursor()
+
+# mycursor.execute("CREATE TABLE Person (name varchar(30), age int, personID int PRIMARY KEY AUTO_INCREMENT)")
+
+# mycursor.execute("DESCRIBE Person")
+
+# mycursor.execute("INSERT INTO Person (name, age) VALUES ('Pranav', 19)")
+# db.commit()
+
+mycursor.execute("SELECT * FROM Person")
+
+for x in mycursor:
+    print(x)
+
